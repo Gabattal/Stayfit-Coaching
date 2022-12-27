f1b01fba4c9f0ee05ce7f11970dafe791cfac2f6
Gabriel Attal <gab-attal@hotmail.fr>
2022-12-27 19:17:43 +0100

    refactor: rename SAddCoach

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import SConnectionLayout from "./pages/connection/SLayout.vue";
 import SHomeLayout from "@/pages/home/SHomeLayout.vue";
 import SAdminLayout from "@/pages/admin/SAdminLayout.vue";
 import SaddCustomer from "@/pages/add-customer/SaddCustomer.vue";
-import SAddCoach from "@/pages/add-coach/SaddCoach.vue";
+import SAddCoach from "@/pages/add-coach/SAddCoach.vue";
 import SCoachLayout from "@/pages/coach/SCoachLayout.vue";
 
 export const router = createRouter({
